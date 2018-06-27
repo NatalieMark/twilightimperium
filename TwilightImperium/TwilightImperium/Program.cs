a4c9663560a3584fdff75f104212e1638fdf6029
NatalieMark <natalies2009@hotmail.com>
2018-06-27 12:47:34 +0200

    first

---
+++ TwilightImperium/TwilightImperium/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TwilightImperium
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TwilightImperium/TwilightImperium/Unit.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace TwilightImperium
+{
+    public class Unit
+    {
+        int _type, 
+            _resourceCost, 
+            _compatValue, 
+            _ovementSpeed, 
+            _capacity;
+        Player _player;
+
+        public Unit(int type, int resourceCost, int compatValue, int ovementSpeed, int capacity, Player player)
+        {
+        }
+
+        public int Type
+        {
+            get { return _type; }
+            set
+            {
+                if (TypeIsValid(value))
+                {
+                    _type = value;
+                }
+            }
+        }
+
+        public bool TypeIsValid(int givenType)
+        {   //unit can be 4 different types
+            return (givenType >= 0 && givenType <= 3);
+        }
+    }
+}
