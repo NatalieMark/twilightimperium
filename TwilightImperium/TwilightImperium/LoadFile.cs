a1bf2ee5fd1090365ced32a8b127d0a135567ca0
Natalie Mark <natalies2005@hotmail.com>
2018-07-07 18:42:25 +0200

    Made some into LoadFile, but has yet to test it

--- TwilightImperium/TwilightImperium/LoadFile.cs
+++ TwilightImperium/TwilightImperium/LoadFile.cs
@@ -1,13 +1,57 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
 namespace TwilightImperium
 {
     public class LoadFile
     {
         string _filename;
 
-        public LoadFile()
+        public List<string[]> LoadedLines(string fileName) //filname can only be units, plantes or races
         {
+            StreamReader file = new StreamReader(fileName + ".rtf");
+            string Header = file.ReadLine();
+            
+            if (fileName == Header) //example, fileName = units, then Header will be the first line in the units.rtf file, which is units.
+            {
+                List<string[]> listOfLines = new List<string[]>();
+                string line;
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    listOfLines.Add(line.Split(';'));
+                }
+                return listOfLines;
+            }
+            else
+                throw new Exception("The file Is not the correct one!!! Check mistakes");
+        }
+        public bool CheckWhichIfUnits(string fileName)
+        {
+            if (fileName + ".rtf" == "units.rtf")
+                return true;
+            else
+                return false;
         }
 
+        /*
+        bool CheckWhichIfPlanets(string fileName)
+        {
+            if (fileName == "planets.rtf")
+                return true;
+            else
+                return false;
+        }
+
+        bool CheckWhichIfRaces(string fileName)
+        {
+            if (fileName == "Races.rtf")
+                return true;
+            else
+                return false;
+        }
+        */
     }
 }
--- TwilightImperium/TwilightImperium/Program.cs
+++ TwilightImperium/TwilightImperium/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TwilightImperium
 {
     class MainClass
     {
+        LoadFile load = new LoadFile();
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Test!");
+        }
+
+        public bool Heyhey(string s)
+        {
+            return load.CheckWhichIfUnits(s);
+        }
+        void Iest(string s)
+        {
+            List<string[]> l = load.LoadedLines(s);
+            l.ForEach(Console.WriteLine);
         }
     }
 }
